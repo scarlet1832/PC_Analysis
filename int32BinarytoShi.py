afb4875f7a17414655ea24d1dd30e973f9c8f3c7
Wayne <1438119973@qq.com>
2023-12-22 15:35:15 +0800

    Change location

--- int32BinarytoShi.py
+++
@@ -1,8 +0,0 @@
-def toBinary(n):
-    return (toBinary(n // 2) if n > 1 else '') + str(n % 2)
- 
- 
-if __name__ == '__main__':
- 
-    n = 20
-    print(f'The binary representation of {n} is', toBinary(n))
